65fe0ce830b5e60061ff4ab2988e56b6259dec00
Sakir Ali <sakir@quavzent.com>
2021-11-18 00:50:53 +0530

    first commit

---
+++ src/app/students.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentsService {
+  url = 'http://localhost:3000/students';
+  constructor(private http: HttpClient) { }
+
+  listAllStudent(){
+    return this.http.get(this.url);
+  }
+  saveStudentData(data: any){
+    console.log(data);
+    return this.http.post(this.url, data);
+  }
+  deleteStudent(id:any){
+    return this.http.delete( `${this.url}/${id}` );
+  }
+  getStudentById(id:any){
+    return this.http.get( `${this.url}/${id}` );
+  }
+  updateStudentData(id:any,data:any){
+    return this.http.put( `${this.url}/${id}`,data );
+  }
+}
